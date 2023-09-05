7f95ee7b7dc01f42b47b2d2e531fd5e010cc5ab4
Talla Giridhar <giridhar.talla2002@gmail.com>
2023-09-05 08:58:48 +0530

    add basic pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,20 @@
 <script>
 	import Button from "../components/common/Button.svelte";
+	import { formatDate } from '$lib/utils'
 
+	export let data;
 </script>
 
 <Button class="text-sm sm:text-base " type="button" variant="outline">Text outline</Button>
+
+<section>
+	<ul class="posts">
+		{#each data.posts as post}
+			<li class="post">
+				<a href={post.slug} class="title">{post.title}</a>
+				<p class="date">{formatDate(post.date)}</p>
+				<p class="description">{post.description}</p>
+			</li>
+		{/each}
+	</ul>
+</section>
